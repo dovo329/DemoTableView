afbd506c1b2893b543cc5a8e284b7320551a5322
Taylor <trm36@humboldt.edu>
2015-06-11 09:53:23 -0600

    Initial Commit

---
+++ DemoTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTableView
+//
+//  Created by TRM on 6/11/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTableView
+//
+//  Created by TRM on 6/11/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
